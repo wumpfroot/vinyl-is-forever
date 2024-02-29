2f27d75a6c3519f2e6555e2489456a34bc7cae7b
wumpfroot <markuskojo@msn.com>
2024-02-29 21:46:30 +0000

    Added a basic fetch with Axios to show all vinyls with info and cover images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
+
+interface Vinyl {
+	_id: string;
+	title: string;
+	artist: string;
+	genres: string[];
+	release: string;
+	coverImg: string;
+}
+
 function App() {
+	const [vinyls, setVinyls] = useState<Vinyl[]>([]);
+
+	useEffect(() => {
+		const fetchVinyls = async () => {
+			try {
+				const response: AxiosResponse = await axios.get("http://localhost:5555/vinyls");
+				setVinyls(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchVinyls();
+	}, []);
+
+	const formatReleaseDate = (release: string): string => {
+		const options = { year: "numeric", month: "2-digit", day: "2-digit" } as Intl.DateTimeFormatOptions;
+		return new Date(release).toLocaleDateString("en-GB", options);
+	};
+
 	return (
 		<>
-			<h1 className="text-4xl">Hello Vite!</h1>
+			<h1 className="text-4xl">Vinyl Collection</h1>
+			<div>
+				{vinyls.map((vinyl) => (
+					<div className="text-2xl my-5" key={vinyl._id}>
+						<h2>
+							{vinyl.artist} - {vinyl.title} ({formatReleaseDate(vinyl.release)})
+						</h2>
+						<img className="w-56 h-auto" src={vinyl.coverImg} alt={`Cover image of ${vinyl.title} by ${vinyl.artist}`} />
+					</div>
+				))}
+			</div>
 		</>
 	);
 }
