dbf35cc72f62211f8dc1da666f016a3fe8b42f27
wumpfroot <markuskojo@msn.com>
2024-03-02 02:33:41 +0000

    Styled a basic album cover grid with hover animations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,24 +25,26 @@ function App() {
 		fetchVinyls();
 	}, []);
 
-	const formatReleaseDate = (release: string): string => {
-		const date = new Date(release);
-		const day = date.getDate().toString().padStart(2, "0");
-		const month = (date.getMonth() + 1).toString().padStart(2, "0");
-		const year = date.getFullYear();
-		return `${day}.${month}.${year}`;
-	};
+	// USE LATER
+	// const formatReleaseDate = (release: string): string => {
+	// 	const date = new Date(release);
+	// 	const day = date.getDate().toString().padStart(2, "0");
+	// 	const month = (date.getMonth() + 1).toString().padStart(2, "0");
+	// 	const year = date.getFullYear();
+	// 	return `${day}.${month}.${year}`;
+	// };
 
 	return (
 		<>
 			<h1 className="text-4xl">Vinyl Collection</h1>
-			<div>
+			<div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center">
 				{vinyls.map((vinyl) => (
-					<div className="text-2xl my-5" key={vinyl._id}>
-						<h2>
-							{vinyl.artist} - {vinyl.title} ({formatReleaseDate(vinyl.release)})
-						</h2>
-						<img className="w-56 h-auto" src={vinyl.coverImg} alt={`Cover image of ${vinyl.title} by ${vinyl.artist}`} />
+					<div key={vinyl._id} className="group text-2xl my-5 relative w-56 text-center cursor-pointer">
+						<img className="w-full h-auto hover:scale-110 filter hover:brightness-50 ease-in duration-200" src={vinyl.coverImg} alt={`Cover image of ${vinyl.title} by ${vinyl.artist}`} />
+
+						<p className="select-none pointer-events-none absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] opacity-0 group-hover:opacity-100 ease-in duration-200">
+							{vinyl.artist} - {vinyl.title}
+						</p>
 					</div>
 				))}
 			</div>
